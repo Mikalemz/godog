25be3d2ce9aac449a6ec08890a9c1445d398201a
María González Bermúdez <mikalemx@gmail.com>
2026-02-13 20:36:54 +0100

    levels interface

--- Scripts/interfaces/options.gd
+++ Scripts/interfaces/options.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/interfaces/menu.tscn")
 
 func _on_master_vol_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value/4))
